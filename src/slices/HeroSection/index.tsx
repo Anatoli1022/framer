54067222c88f7bcf8c637098cbdb4234cfa97a66
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-04-05 00:21:32 +0300

    feat: adds new styles and image in section hero

--- src/slices/HeroSection/index.tsx
+++ src/slices/HeroSection/index.tsx
@@ -1,19 +1,18 @@
 import { Content } from '@prismicio/client';
-import { SliceComponentProps } from '@prismicio/react';
+import { PrismicImage, SliceComponentProps } from '@prismicio/react';
 
 import { PrismicText, PrismicLink } from '@prismicio/react';
 
 export type HeroSectionProps = SliceComponentProps<Content.HeroSecrionSlice>;
 
 const HeroSection = ({ slice }: HeroSectionProps): JSX.Element => {
-  console.log(slice);
   return (
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      className="flex justify-between mt-8 bg-gradient-to-bl from-blue-100 to-blue-900 "
+      className="mt-8 bg-gradient-to-b from-white to-blue-700 "
     >
-      <div className="max-w-7xl ml-auto mr-auto ">
+      <div className="max-w-7xl ml-auto mr-auto flex justify-between items-center gap-x-6">
         <div className="max-w-lg">
           <span className="text-xs font-medium border border-solid border-gray-300 rounded-lg py-2 px-3">
             {slice.primary.eyebrowheadline}
@@ -22,14 +21,14 @@ const HeroSection = ({ slice }: HeroSectionProps): JSX.Element => {
             <PrismicText field={slice.primary.title} />
           </h1>
 
-          <p className="text-2xl mt-7 text-base font-normal">
+          <p className="text-2xl mt-7   font-normal">
             <PrismicText field={slice.primary.description} />
           </p>
 
           <div className="mt-7">
             <PrismicLink
               field={slice.primary.calltoactionlink}
-              className="text-base font-medium text-white py-3 px-4 rounded-lg bg-black hover:text-black hover:bg-white transition duration-300 border border-black border-solid border-1"
+              className="text-base font-medium text-white py-3 px-4 rounded-lg bg-black hover:text-black hover:bg-transparent transition duration-300 border border-black border-solid border-1"
             >
               {slice.primary.calltoactionlabel}
             </PrismicLink>
@@ -41,7 +40,9 @@ const HeroSection = ({ slice }: HeroSectionProps): JSX.Element => {
             </PrismicLink>
           </div>
         </div>
-        <div>af</div>
+        <div>
+          <PrismicImage field={slice.primary.image} />
+        </div>
       </div>
     </section>
   );
