2bc1492aac1b1bf5ae0af4a6efb3ab5b900c9171
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-04-07 02:13:07 +0300

    feat: adds  SignUpSection and StreamlinedSection

---
+++ src/slices/SignUpSection/index.tsx
@@ -0,0 +1,45 @@
+import { Content } from '@prismicio/client';
+import { SliceComponentProps } from '@prismicio/react';
+import { PrismicText, PrismicImage, PrismicLink } from '@prismicio/react';
+
+export type SignUpSectionProps =
+  SliceComponentProps<Content.SignUpSectionSlice>;
+
+const SignUpSection = ({ slice }: SignUpSectionProps): JSX.Element => {
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+      className="bg-gradient-to-b from-white bg-indigo-200 mt-28 pb-28"
+    >
+      <div className="max-w-7xl ml-auto mr-auto text-center">
+        <h2 className="text-6xl font-bold">
+          <PrismicText field={slice.primary.title} />
+        </h2>
+        <p className="text-xl font-normal mt-5 ml-auto mr-auto max-w-xl ">
+          <PrismicText field={slice.primary.text} />
+        </p>
+        <div className="mt-10 flex justify-center">
+          <PrismicLink
+            field={slice.primary.get}
+            className="text-base font-medium text-white py-3 px-4 rounded-lg bg-black hover:text-black hover:bg-transparent transition duration-300 border border-black border-solid border-1"
+          >
+            {slice.primary.getlabel}
+          </PrismicLink>
+          <PrismicLink
+            field={slice.primary.learn}
+            className="flex items-center ml-4 text-base font-medium"
+          >
+            {slice.primary.labellearn}
+            <PrismicImage
+              field={slice.primary.arrow}
+              className="align-middle block ml-2"
+            />
+          </PrismicLink>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SignUpSection;
---
+++ src/slices/StreamlinedSection/index.tsx
@@ -0,0 +1,51 @@
+import { Content } from '@prismicio/client';
+import { SliceComponentProps } from '@prismicio/react';
+import { PrismicText } from '@prismicio/react';
+/**
+ * Props for `StreamlinedSection`.
+ */
+export type StreamlinedSectionProps =
+  SliceComponentProps<Content.StreamlinedSectionSlice>;
+
+/**
+ * Component for "StreamlinedSection" Slices.
+ */
+const StreamlinedSection = ({
+  slice,
+}: StreamlinedSectionProps): JSX.Element => {
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+      className="mt-24"
+    >
+      <div className="max-w-7xl ml-auto mr-auto text-center">
+        <span className="text-xs font-medium py-2.5 px-3.5 border border-solid border-gray-300 rounded-lg">
+          {slice.primary.span}
+        </span>{' '}
+        <h2 className="text-6xl font-bold mt-6 ml-auto mr-auto max-w-2xl">
+          <PrismicText field={slice.primary.title} />
+        </h2>
+        <p className="text-xl font-normal mt-5 ml-auto mr-auto max-w-lg">
+          <PrismicText field={slice.primary.text} />
+        </p>
+        <ul className="flex justify-center gap-x-4 mt-16">
+          {slice.items.map((item, i) => {
+            return (
+              <li key={i} className="p-10 rounded-3xl border border-solid border-gray-100 shadow-xl">
+                <h3 className="text-2xl font-bold">
+                  <PrismicText field={item.title} />
+                </h3>
+                <p className="text-xl font-normal mt-4 ml-auto mr-auto max-w-96">
+                  <PrismicText field={item.text} />
+                </p>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default StreamlinedSection;
