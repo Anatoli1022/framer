b1615e45d0b392c72226431133fe39998395d544
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-04-10 00:14:03 +0300

    feat: adds logo and button in navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,5 +1,5 @@
 import { Client, Content, isFilled } from '@prismicio/client';
-import { PrismicLink } from '@prismicio/react';
+import { PrismicLink, PrismicImage } from '@prismicio/react';
 
 export const Navigation = async ({
   client,
@@ -9,15 +9,17 @@ export const Navigation = async ({
   const navigation = await client.getSingle('navigation');
 
   return (
-    <nav className="pt-5 max-w-7xl ml-auto mr-auto flex justify-between">
-      <div>logo</div>
-      <ul className="flex gap-x-6">
+    <nav className="pt-5 max-w-7xl ml-auto mr-auto flex justify-between items-center">
+      <PrismicLink field={navigation.data.logo_link}>
+        <PrismicImage field={navigation.data.logo} />
+      </PrismicLink>
+      <ul className="flex gap-x-6 items-center">
         {isFilled.group(navigation.data.menu_items) &&
           navigation.data.menu_items.map((item) => {
             return (
               <li key={item.label}>
                 <PrismicLink
-                  className="text-black opacity-60 text-base"
+                  className="text-black opacity-60 text-base hover:opacity-100 transition duration-300"
                   field={item.link}
                 >
                   {item.label}
@@ -25,7 +27,14 @@ export const Navigation = async ({
               </li>
             );
           })}
-          <li>button</li>
+        <li>
+          <PrismicLink
+            className="text-white text-base px-4 py-2.5 border border-solid border-black rounded-xl bg-black hover:bg-transparent hover:text-black transition duration-300"
+            field={navigation.data.get_free}
+          >
+            {navigation.data.label_get_free}
+          </PrismicLink>
+        </li>
       </ul>
     </nav>
   );
