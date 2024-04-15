3cfcda5e2f71fe17a8c30024db917d99ece1f6f0
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-04-16 00:02:22 +0300

    feat: adds footer and footer list item

---
+++ src/components/Footer.tsx
@@ -0,0 +1,37 @@
+import { components } from '@/slices';
+import { SliceZone } from '@prismicio/react';
+import { createClient } from '@/prismicio';
+import { PrismicText, PrismicLink, PrismicImage } from '@prismicio/react';
+export const Footer = async () => {
+  const client = createClient();
+  const data = await client.getSingle('footer');
+
+  return (
+    <footer className="pt-10 pb-12 bg-black">
+      <div className="  max-w-7xl ml-auto mr-auto flex justify-between">
+        <div className="max-w-60">
+          <PrismicLink field={data.data.logo_link}>
+            <PrismicImage field={data.data.logo} />
+          </PrismicLink>
+          <p className="text-gray-400 text-sm mt-5">
+            <PrismicText field={data.data.text} />
+          </p>
+          <ul className="mt-28 flex">
+            {data.data.social_links.map((item, i) => {
+              return (
+                <li key={i} className="ml-4 first:ml-0">
+                  <PrismicLink field={item.social_link}>
+                    <PrismicImage field={item.image_link} className="w-6" />
+                  </PrismicLink>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <div className="flex">
+          <SliceZone slices={data.data.slices} components={components} />
+        </div>
+      </div>
+    </footer>
+  );
+};
---
+++ src/slices/FooterItem/index.tsx
@@ -0,0 +1,56 @@
+// import { Content } from "@prismicio/client";
+// import { SliceComponentProps } from "@prismicio/react";
+
+// /**
+//  * Props for `FooterItem`.
+//  */
+// export type FooterItemProps = SliceComponentProps<Content.FooterItemSlice>;
+
+// /**
+//  * Component for "FooterItem" Slices.
+//  */
+// const FooterItem = ({ slice }: FooterItemProps): JSX.Element => {
+//   return (
+//     <section
+//       data-slice-type={slice.slice_type}
+//       data-slice-variation={slice.variation}
+//     >
+//       Placeholder component for footer_item (variation: {slice.variation})
+//       Slices
+//     </section>
+//   );
+// };
+
+// export default FooterItem;
+import { Content } from '@prismicio/client';
+import { SliceComponentProps } from '@prismicio/react';
+import { PrismicText, PrismicLink } from '@prismicio/react';
+
+/**
+ * Props for `FooterItem`.
+ */
+export type FooterItemProps = SliceComponentProps<Content.FooterItemSlice>;
+
+/**
+ * Component for "FooterItem" Slices.
+ */
+const FooterItem = ({ slice }: FooterItemProps): JSX.Element => {
+  return (
+    <ul className='ml-12 first:ml-0'>
+      <li>
+        <h3 className='text-white'>
+          <PrismicText field={slice.primary.title} />
+        </h3>
+      </li>
+      {slice.items.map((item, i) => {
+        return (
+          <li key={i} className='mt-5 first:mt-0'>
+            <PrismicLink className='text-gray-500 text-sm hover:text-gray-300 duration-200 ' field={item.link}>{item.link_label}</PrismicLink>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default FooterItem;
