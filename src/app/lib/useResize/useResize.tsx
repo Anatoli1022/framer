367a9f225db57e69685dca4eef9d2ad2015f0cf0
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2024-06-11 22:58:34 +0300

    fix: fix useResize 2

---
+++ src/app/lib/useResize/useResize.tsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+import {
+  SCREEN_SM,
+  SCREEN_MD,
+  SCREEN_LG,
+  SCREEN_XL,
+  SCREEN_XXL,
+} from './const-breakpoints';
+
+export const useResize = () => {
+  const [width, setWidth] = useState<number | null>(null);
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+
+    const handleResize = (event: UIEvent) => {
+      if (event.target instanceof Window) {
+        setWidth(event.target.innerWidth);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return {
+    width,
+    isScreenSm: width ? width >= SCREEN_SM : false,
+    isScreenMd: width ? width >= SCREEN_MD : false,
+    isScreenLg: width ? width >= SCREEN_LG : false,
+    isScreenXl: width ? width >= SCREEN_XL : false,
+    isScreenXxl: width ? width >= SCREEN_XXL : false,
+  };
+};
